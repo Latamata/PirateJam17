fb6f2c24f7e64aad032860bee439dae337f3873d
Latamata <chancewebber@gmail.com>
2025-07-22 12:26:09 -0700

    Minor changes
    setup up e key to trigger dialogue with NPC.

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -5,13 +5,7 @@ extends Node2D
 @onready var camera_2d: Camera2D = $player/Camera2D
 @onready var player: CharacterBody2D = $player
 @onready var dialogue_manager: Node2D = $dialogue_manager
-#var dialogue_box: PackedScene = preload("res://addons/dialogue/scenes/dialogue.tscn")
-
-#func _ready() -> void:
-	#$dialogue_manager.start('dialogue')
-#func _process(delta: float) -> void:
-	#if $dialogue_manager != null:
-		#$dialogue_manager.global_position = player.global_position
+var player_dialogue_range = false
 
 func split_book_spawns(position_of_book):
 	var musketman_instance = crap_page.instantiate()
@@ -22,7 +16,11 @@ func _on_enemy_book_split_into_pages(book_position: Vector2) -> void:
 	split_book_spawns(book_position)
 
 func _on_npc_player_pressed() -> void:
-	#var new_dialogue = dialogue_box.instantiate()
-	#$player/dialogue_node.add_child(new_dialogue)
-	#new_dialogue.start('dialogue')
-	$dialogue_manager.start('dialogue')
+	player_dialogue_range = true
+
+func _input(event):
+	if Input.is_action_pressed('player_action'):
+		if player_dialogue_range:
+			#print('in range of dialogeu')
+			$dialogue_manager.start('dialogue')
+			player_dialogue_range = false
--- scripts/npc_script.gd
+++ scripts/npc_script.gd
@@ -3,8 +3,6 @@ extends CharacterBody2D
 signal player_pressed
 @onready var press_to_talk_text: RichTextLabel = $press_to_talk_text
 
-
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group('player'):
 		print('player in range')

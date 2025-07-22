6b4317af0c7921746be83ff6b68951ee100069d0
Latamata <chancewebber@gmail.com>
2025-07-21 20:33:09 -0700

    Minor changes
    instead of instantiating scene, I used the dialogue manager.

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -5,7 +5,7 @@ extends Node2D
 @onready var camera_2d: Camera2D = $player/Camera2D
 @onready var player: CharacterBody2D = $player
 @onready var dialogue_manager: Node2D = $dialogue_manager
-var dialogue_box: PackedScene = preload("res://addons/dialogue/scenes/dialogue_manager.tscn")
+#var dialogue_box: PackedScene = preload("res://addons/dialogue/scenes/dialogue.tscn")
 
 #func _ready() -> void:
 	#$dialogue_manager.start('dialogue')
@@ -20,10 +20,9 @@ func split_book_spawns(position_of_book):
 
 func _on_enemy_book_split_into_pages(book_position: Vector2) -> void:
 	split_book_spawns(book_position)
-	
 
 func _on_npc_player_pressed() -> void:
-	print(dialogue_manager)
-	var new_dialogue = dialogue_box.instantiate()
-	$player/dialogue_node.add_child(new_dialogue)
-	new_dialogue.start('dialogue')
+	#var new_dialogue = dialogue_box.instantiate()
+	#$player/dialogue_node.add_child(new_dialogue)
+	#new_dialogue.start('dialogue')
+	$dialogue_manager.start('dialogue')

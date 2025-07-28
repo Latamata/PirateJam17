0f8a72154accec52eff1b912c619ee72724c531c
Latamata <chancewebber@gmail.com>
2025-07-28 13:55:18 -0700

    minor changes
    set up the enemies flow spawn on mountian.

--- scripts/enemy_book.gd
+++ scripts/enemy_book.gd
@@ -1,36 +1,28 @@
 extends CharacterBody2D
 
-#signal split_into_pages(book_position: Vector2)
-# Variables
-var direction = Vector2.RIGHT
-var SPEED = 50.0
+const SPEED = 50
+const GRAVITY = 400
+var direction := -1  
 var target
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-
-func _process(_delta):
+func _process(delta):
+	velocity.y += GRAVITY * delta
 	if ray_cast_left.is_colliding():
 		var collider = ray_cast_left.get_collider()
 		if collider.is_in_group('player'):
 			collider.player_died_function()
-		direction *= -1
-
+		direction = 1
 	if ray_cast_right.is_colliding():
 		var collider = ray_cast_right.get_collider()
 		if collider.is_in_group('player'):
 			collider.player_died_function()
-		direction *= -1
-
-	velocity = direction * SPEED
-
-	# Flip sprite based on direction
-	#animated_sprite_2d.flip_h = direction < 0
-
-	# Play walk animation
+		direction = -1
+	velocity.x = direction * SPEED
+	animated_sprite_2d.flip_h = direction < 0
 	animated_sprite_2d.play("run")
-
 	move_and_slide()
 
 func GDD_broken():
--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -6,17 +6,15 @@ var player_dialogue_range = false
 const DIALOGUE_MANAGER = preload("res://addons/dialogue/scenes/dialogue_manager.tscn")
 const DIALOGUE = preload("res://addons/dialogue/scenes/dialogue.tscn")
 const TEST_DIALOGUE = preload("res://scenes/test_dialogue.tscn")
+const ENEMY_SPAWN = preload("res://scenes/enemy_zero.tscn")
+@onready var enemy_spawn: Marker2D = $enemy_spawn
 
 func _on_npc_player_pressed() -> void:
 	player_dialogue_range = true
 
 func _input(_event):
 	if Input.is_action_just_pressed('player_action'):
-		print('running')
-		#if player_dialogue_range:
-			#player_dialogue_range = false
 		talk_to_npc()
-
 
 var active_dialogue_manager: Node = null
 func talk_to_npc():
@@ -28,9 +26,11 @@ func talk_to_npc():
 	active_dialogue_manager = manager
 
 func _on_world_boundry_body_entered(body: Node2D) -> void:
+	if body.is_in_group('enemy'):
+		body.queue_free()
 	if body.is_in_group('player'):
 		body.global_position = $player_spawn.global_position
-
+	
 func _on_npc_player_exited() -> void:
 	player_dialogue_range = false
 	if active_dialogue_manager:
@@ -39,3 +39,8 @@ func _on_npc_player_exited() -> void:
 
 func _on_player_player_died() -> void:
 	player.global_position = $player_spawn.global_position
+
+func _on_timer_timeout() -> void:
+	var enemy_instance = ENEMY_SPAWN.instantiate()
+	enemy_instance.global_position = enemy_spawn.global_position
+	get_tree().current_scene.add_child(enemy_instance)
